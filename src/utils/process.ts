16bd0918ea690ca7efb35e9c2f9d770abfa921ad
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-06 11:59:07 +0530

    feat: update process detection

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,10 +1,9 @@
 import { execa } from "./execa";
 
 export const isProcessRunning = async (name: string) => {
-  const { stdout } = await execa("tasklist");
-  const lines = stdout.trim().split("\n");
-  const processes = lines.slice(2);
-  const parsed = processes.map(it => it.match(/(.+?)[\s]+?(\d+)/));
-  const found = parsed.find(it => it?.[1]?.toLowerCase() === name);
-  return Boolean(found);
+  const { stdout } = await execa(`tasklist /FI "ImageName eq ${name}"`);
+  if (!stdout) return false;
+  if (stdout.includes("No tasks are running")) return true;
+
+  return true;
 };
