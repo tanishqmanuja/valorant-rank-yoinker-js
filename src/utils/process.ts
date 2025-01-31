022d458f61a248b4f6236c20f8fefb4a5fe2d30e
tanishqmanuja <tanishqmanuja@gmail.com>
2025-01-31 12:09:19 +0530

    fix: process detection

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -3,7 +3,7 @@ import { execa } from "./execa";
 export const isProcessRunning = async (name: string) => {
   const { stdout } = await execa(`tasklist /FI "ImageName eq ${name}"`);
   if (!stdout) return false;
-  if (stdout.includes("No tasks are running")) return true;
+  if (stdout.includes("No tasks are running")) return false;
 
   return true;
 };
