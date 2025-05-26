184d4d00ceb90868f8a017e170721aaa7d60bd18
tanishqmanuja <tanishqmanuja@gmail.com>
2025-05-26 17:46:33 +0530

    fix: use csv mode in tasklist
    thanks to @pintoso for the idea

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,9 +1,6 @@
 import { execa } from "./execa";
 
 export const isProcessRunning = async (name: string) => {
-  const { stdout } = await execa(`tasklist /FI "ImageName eq ${name}"`);
-  if (!stdout) return false;
-  if (stdout.includes("No tasks are running")) return false;
-
-  return true;
+  const { stdout } = await execa(`tasklist /FO csv /FI "ImageName eq ${name}"`);
+  return stdout?.toLowerCase().includes(name.toLowerCase());
 };
