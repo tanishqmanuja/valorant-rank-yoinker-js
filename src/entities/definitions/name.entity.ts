e24cd1f7ddecf230442ce3a901bb4b5b850004cc
tanishqmanuja <tanishqmanuja@gmail.com>
2026-02-08 12:40:37 +0530

    feat: get hidden names from db

--- src/entities/definitions/name.entity.ts
+++ src/entities/definitions/name.entity.ts
@@ -1,18 +1,48 @@
+import { db } from "~/db";
+import { playerNames } from "~/db/schema";
 import { definePlayerEntity } from "~/entities/types/player-entity.interface";
 
 /** @RequestFactor 0 */
 export const NameEntity = definePlayerEntity({
   id: "name",
   hooks: {
-    onState: ({ player, data }) => {
-      const name = data.prefetched.names.find(
+    onState: async ({ player, data }) => {
+      const nameObject = data.prefetched.names.find(
         p => p.Subject === player.Subject,
       )!;
 
+      const hasName =
+        nameObject.GameName.trim() !== "" && nameObject.TagLine.trim() !== "";
+
+      if (hasName) {
+        db.update(playerNames).set({
+          id: player.Subject,
+          name: nameObject.GameName,
+          tag: nameObject.TagLine,
+        });
+
+        return {
+          value: `${nameObject.GameName}#${nameObject.TagLine}`,
+          isHidden: player.PlayerIdentity.Incognito,
+        };
+      }
+
       return {
-        value: `${name.GameName}#${name.TagLine}`,
+        value: await tryPlayerNameFromDB(player.Subject),
         isHidden: player.PlayerIdentity.Incognito,
       };
     },
   },
 });
+
+async function tryPlayerNameFromDB(puuid: string): Promise<string> {
+  const nameRecord = await db.query.playerNames.findFirst({
+    where: (r, { eq }) => eq(r.id, puuid),
+  });
+
+  if (nameRecord) {
+    return nameRecord.name + "#" + nameRecord.tag;
+  } else {
+    return "";
+  }
+}
